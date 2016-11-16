9b9265b9b1983efa594efc07c16f9de05ab99c41
MaximeMorel <maxime.morel69@gmail.com>
2016-11-16 17:08:06 +0000

    Initial script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MediaMinify
 Script to minify a media collection of images and videos. They will be converted to jpg and h265 mkv.
+
+Usage:
+./minify src dst [quality]
+
+If using relative paths, prepend dst with ../
+
---
+++ minify.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+# Minimize picture and video script
+# Maxime Morel - maxime@mmorel.eu
+# 2015/12/17
+#
+# This script will reduce size and homogenize media library.
+# Convert to jpg, quality 85
+# Convert to mkv
+#
+# Requirements:
+# python
+# convert (imagemagick)
+# ffmeg
+# ufraw-batch (gimp-ufraw), for CR2 convertion
+
+# Usage:
+# ./minify.py | tee logs/minify-$(date +%F-%T).log
+
+import sys
+import os
+import time
+import subprocess
+import shutil
+
+print('Picture and video minification script')
+
+base = 'photos_originals'
+if len(sys.argv) >= 2:
+        base = sys.argv[1]
+
+newbase = '../photos_minified'
+if len(sys.argv) >= 3:
+        newbase = sys.argv[2]
+
+print(os.path.join(os.getcwd(), base))
+print(os.path.join(os.getcwd(), base, newbase))
+
+print('Proceed? ')
+res = input()
+if res != 'y':
+        sys.exit()
+
+os.chdir(base)
+
+if not os.path.isdir(newbase):
+	os.mkdir(newbase)
+
+for root, dirs, files in os.walk('.'):
+	for dir in dirs:
+		fullsrcdir = os.path.join(root, dir)
+		fulldstdir = os.path.join(newbase, fullsrcdir)
+		print('mkdir: ', fullsrcdir, '->', fulldstdir)
+		if not os.path.isdir(fulldstdir):
+			os.mkdir(fulldstdir)
+
+	for file in files:
+		fullsrcpath = os.path.join(root, file)
+		basefile, extension = os.path.splitext(file)
+		interdstpath = os.path.join(newbase, root, basefile)
+		fulldstpath = interdstpath + extension
+		extension = extension.lower()
+		if basefile.startswith('.'):
+			print('dot file: ', fullsrcpath)
+			continue
+		if extension in ['.jpg', '.jpeg', '.bmp', '.png', '.cr2']:
+			fulldstpath = interdstpath + '.jpg'
+			if not os.path.exists(fulldstpath):
+				subprocess.run(["convert", "-quality", "85", fullsrcpath, fulldstpath])
+				srcsize = os.stat(fullsrcpath).st_size
+				try:
+					dstsize = os.stat(fulldstpath).st_size
+				except BaseException as err:
+					dstsize = 0
+					print('Error: ', fullsrcpath, '->', fulldstpath, ' - ', err)
+					#pass
+				ratio = dstsize / srcsize
+				print('convert: ', os.path.join(fullsrcpath), '->', fulldstpath, "{0:.2f}".format(ratio))
+				if ratio >= 1.0:
+					print('  keep original file')
+					shutil.copy2(fullsrcpath, fulldstpath)
+			else:
+				print('skip: ', os.path.join(fullsrcpath), '->', fulldstpath)
+		elif extension in ['.mov', '.mp4', '.wmv', '.mod', '.mpg', '.avi', '.mts', '.mkv']:
+			fulldstpath = interdstpath + '.mkv'
+			if not os.path.exists(fulldstpath):
+				subprocess.run(["ffmpeg", "-y", "-i", fullsrcpath, "-vf", "yadif=0:-1:0",  fulldstpath])
+				srcsize = os.stat(fullsrcpath).st_size
+				try:
+					dstsize = os.stat(fulldstpath).st_size
+				except BaseException as err:
+					dstsize = 0
+					print('Error: ', fullsrcpath, '->', fulldstpath, ' - ', err)
+					#pass
+				ratio = dstsize / srcsize
+				print('ffmpeg: ', os.path.join(fullsrcpath), '->', fulldstpath, "{0:.2f}".format(ratio))
+				#time.sleep(1)
+			else:
+				print('skip: ', os.path.join(fullsrcpath), '->', fulldstpath)
+		elif extension in ['.zip', '.pdf', '.txt', '.sh', '.thm']:
+			print('copy: ', os.path.join(fullsrcpath), '->', fulldstpath)
+			subprocess.run(["cp", "-a", fullsrcpath, fulldstpath])
+		else:
+			print('unknown ', fullsrcpath)
+
